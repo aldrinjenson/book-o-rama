ff59f61c5cba243d658f1669b06098c96f4932ac
aldrin163 <aldrinjenson@gmail.com>
2020-05-14 15:51:55 +0530

    Refractoring

--- tabs/HomeTab.js
+++ tabs/HomeTab.js
@@ -1,20 +1,15 @@
-import React, { useState, useEffect, useContext } from "react";
-import { StyleSheet, View, FlatList, ActivityIndicator } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, FlatList, ActivityIndicator } from "react-native";
 import { globalStyles } from "../global/globalStyles";
 import BookCollection from "../components/BookCollection";
 import axios from "axios";
-// import { WishListContext } from "../contexts/wishListContext";
+import { query, baseUrl, apiKey } from "../config/apiKey";
 
 const HomeTab = ({ navigation }) => {
-  // const { getFromAsyncStorage } = useContext(WishListContext); // for getting stored wishList from async storage
-
   const [topBooksList, setTopBooksList] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const query = "lists/overview.json";
-      const baseUrl = "https://api.nytimes.com/svc/books/v3/";
-      const apiKey = "ds3m2kNA6cZBkueVBxgYWA67QEIWA9W6";
       const API_URL = baseUrl + query + "?api-key=" + apiKey;
       try {
         const res = await axios.get(API_URL);
@@ -24,7 +19,6 @@ const HomeTab = ({ navigation }) => {
       }
     };
     fetchData();
-    // getFromAsyncStorage();
   }, []);
 
   return (
@@ -53,5 +47,3 @@ const HomeTab = ({ navigation }) => {
 };
 
 export default HomeTab;
-
-const styles = StyleSheet.create({});
